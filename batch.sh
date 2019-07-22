3134db52acceb53b5667473bb4b08fc39f28a964
m_cre <mc@mchs-u.net>
2019-07-23 00:23:12 +0900

    add pdf2png

--- batch.sh
+++ batch.sh
@@ -10,13 +10,16 @@ cp ./template.tex ./work
 cp ./projects/${1}/novel.txt ./work
 
 echo '# az2tex'
-docker run --rm -it -v $PWD/work:/app listening-paperback-python /bin/sh -c "python az2tex.py" || exit 1
+docker run --rm -it -v $PWD/work:/work listening-paperback-python /bin/sh -c "python az2tex.py" || exit 1
 echo '# tex2pdf'
-docker run --rm -it -v $PWD/work:/workdir paperist/alpine-texlive-ja /bin/bash -c "cd /workdir && uplatex novel.tex && dvipdfmx novel.dvi" || exit 1
+docker run --rm -it -v $PWD/work:/work paperist/alpine-texlive-ja /bin/bash -c "cd /work && uplatex novel.tex && dvipdfmx novel.dvi" || exit 1
+echo '# pdf2png'
+mkdir ./work/pages
+docker run --rm -it -v $PWD/work:/work gkmr/pdf-tools /bin/sh -c "pdftocairo -png -r 200 /work/novel.pdf /work/pages/output" || exit 1
 echo '# tex2ssml'
-docker run --rm -it -v $PWD/work:/app listening-paperback-python /bin/sh -c "python tex2ssml.py" || exit 1
+docker run --rm -it -v $PWD/work:/work listening-paperback-python /bin/sh -c "python tex2ssml.py" || exit 1
 echo '# ssml2voice'
-docker run --rm -it -v $PWD/work:/app listening-paperback-python /bin/sh -c "python ssml2voice.py ${2} ${3}" || exit 1
+docker run --rm -it -v $PWD/work:/work listening-paperback-python /bin/sh -c "python ssml2voice.py ${2} ${3}" || exit 1
 
 echo '# postprocessing'
 cid=`git log -n 1 --format=%h`
@@ -29,6 +32,7 @@ cd ../../../
 
 cp ./work/novel.tex ./projects/${1}/output_${cid}/
 cp ./work/novel.pdf ./projects/${1}/output_${cid}/
+cp ./work/pages/* ./projects/${1}/output_${cid}/pages/
 cp ./work/ssml/* ./projects/${1}/output_${cid}/ssml/
 cp ./work/voices/* ./projects/${1}/output_${cid}/voices/
 cp ./work/marks/* ./projects/${1}/output_${cid}/marks/
