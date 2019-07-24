d18a7334860cd761ee05cd282e93fb65b14b92fa
m_cre <mc@mchs-u.net>
2019-07-24 17:14:43 +0900

    ページ切り替えにフェードイン・アウトを追加

--- src/buildmovie.py
+++ src/buildmovie.py
@@ -1,14 +1,19 @@
 import glob
 
-from moviepy.editor import concatenate_videoclips, afx, vfx
+from moviepy.editor import afx, vfx
 from moviepy.audio.io.AudioFileClip import AudioFileClip
+from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 from moviepy.video.VideoClip import ImageClip
+
+fade_time = 0.15
 
 def main():
     video_clips = []
-    for page in sorted(glob.glob('pages/novel*.png')):
-        video_clips.append(ImageClip(page).set_duration(1))
-    video_clip = concatenate_videoclips(video_clips, method='compose')
+    for i, page in enumerate(sorted(glob.glob('pages/novel*.png'))):
+        duration = 1
+        clip = ImageClip(page).set_start(i * (duration - fade_time)).set_duration(duration).crossfadein(fade_time).crossfadeout(fade_time)
+        video_clips.append(clip)
+    video_clip = CompositeVideoClip(video_clips, bg_color=(255, 255, 255))
 
     music = AudioFileClip('music.mp3')
     music = afx.audio_loop(music, duration=video_clip.duration)
