9a794fe14b1fbddc023e000348cb1218bb2fdda8
m_cre <mc@mchs-u.net>
2019-07-25 00:42:53 +0900

    画質、音質の調整

--- batch.sh
+++ batch.sh
@@ -1,5 +1,6 @@
 echo '# docker-build'
-docker build -t listening-paperback-python ./Dockerfiles/python || exit 1
+docker build -t lp-python ./Dockerfiles/python || exit 1
+docker build -t lp-python-movie ./Dockerfiles/python_movie || exit 1
 
 echo '# preprocessing'
 rm -rf ./work || exit 1
@@ -12,19 +13,19 @@ cp ./materials/fonts/`cat ./projects/${1}/config.json | jq -r .font` ./work/font
 cp ./materials/musics/`cat ./projects/${1}/config.json | jq -r .music` ./work/music.mp3 || exit 1
 
 echo '# az2tex'
-docker run --rm -it -v $PWD/work:/work listening-paperback-python /bin/sh -c "python az2tex.py" || exit 1
+docker run --rm -it -v $PWD/work:/work lp-python /bin/sh -c "python az2tex.py" || exit 1
 echo '# tex2pdf'
 docker run --rm -it -v $PWD/work:/work paperist/alpine-texlive-ja /bin/bash -c "cd /work && uplatex novel.tex && dvipdfmx novel.dvi" || exit 1
 echo '# pdf2png'
 mkdir ./work/pages
 docker run --rm -it -v $PWD/work:/work gkmr/pdf-tools /bin/sh -c "pdftocairo -png -r 200 /work/novel.pdf /work/pages/novel" || exit 1
 echo '# tex2ssml'
-docker run --rm -it -v $PWD/work:/work listening-paperback-python /bin/sh -c "python tex2ssml.py" || exit 1
+docker run --rm -it -v $PWD/work:/work lp-python /bin/sh -c "python tex2ssml.py" || exit 1
 echo '# ssml2voice'
 mkdir ./work/voices ./work/marks & cp ./tmp/voices/* ./work/voices/ & cp ./tmp/marks/* ./work/marks/ || exit 1 # debug
-# docker run --rm -it -v $PWD/work:/work listening-paperback-python /bin/sh -c "python ssml2voice.py ${2} ${3}" || exit 1
+# docker run --rm -it -v $PWD/work:/work lp-python /bin/sh -c "python ssml2voice.py ${2} ${3}" || exit 1
 echo '# buildmovie'
-docker run --rm -it -v $PWD/work:/work listening-paperback-python /bin/sh -c "python buildmovie.py" || exit 1
+docker run --rm -it -v $PWD/work:/work lp-python-movie /bin/sh -c "python3 buildmovie.py" || exit 1
 
 echo '# postprocessing'
 cid=`git log -n 1 --format=%ad-%h --date=format:'%Y%m%d'`
@@ -38,7 +39,7 @@ cd ../../../
 cp ./work/config.json ./projects/${1}/output_${cid}/ || exit 1
 cp ./work/novel.tex ./projects/${1}/output_${cid}/ || exit 1
 cp ./work/novel.pdf ./projects/${1}/output_${cid}/ || exit 1
-cp ./work/novel.avi ./projects/${1}/output_${cid}/ || exit 1
+cp ./work/novel.mp4 ./projects/${1}/output_${cid}/ || exit 1
 
 cp ./work/pages/* ./projects/${1}/output_${cid}/pages/ || exit 1
 cp ./work/ssml/* ./projects/${1}/output_${cid}/ssml/ || exit 1
--- src/buildmovie.py
+++ src/buildmovie.py
@@ -6,7 +6,7 @@ from moviepy.editor import afx, vfx, CompositeVideoClip, CompositeAudioClip, Ima
 FADE_TIME = 0.15
 START_VOICE_INTERVAL = 2
 VOICE_INTERVAL = 1
-MUSIC_VOLUME = 0.1
+MUSIC_VOLUME = 0.15
 
 def main():
     voice_clips = []
@@ -21,18 +21,23 @@ def main():
     video_clips = []
     for i, page in enumerate(sorted(glob.glob('pages/novel*.png'))):
         duration = 1
-        clip = ImageClip(page).set_start(i * (duration - FADE_TIME)).set_duration(duration).crossfadein(FADE_TIME).crossfadeout(FADE_TIME)
+        clip = ImageClip(page) \
+            .set_start(i * (duration - FADE_TIME)) \
+            .set_duration(duration) \
+            .crossfadein(FADE_TIME) \
+            .crossfadeout(FADE_TIME)
         video_clips.append(clip)
     video_clip = CompositeVideoClip(video_clips, bg_color=(255, 255, 255))
 
-    music_clip = AudioFileClip('music.mp3')\
-        .audio_loop(duration=video_clip.duration)\
-        .audio_fadeout(duration=5)
+    music_clip = AudioFileClip('music.mp3') \
+        .audio_loop(duration=video_clip.duration) \
+        .audio_fadeout(duration=5) \
+        .volumex(MUSIC_VOLUME)
 
-    audio_clip = CompositeAudioClip([voice_clip, music_clip.fx(afx.volumex, MUSIC_VOLUME)])
+    audio_clip = CompositeAudioClip([voice_clip, music_clip])
 
     video_clip = video_clip.set_audio(audio_clip)
-    video_clip.write_videofile('novel.avi', fps=30, codec='libx264')
+    video_clip.write_videofile('novel.mp4', fps=30, codec='libx264', audio_codec='libfdk_aac', audio_bitrate='384k')
 
 if __name__ == '__main__':
     main()
