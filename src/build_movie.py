7466f621425cb9c0d7623e7b29238f2baa01a19f
m_cre <mc@mchs-u.net>
2019-07-25 22:01:27 +0900

    微調整

--- src/build_movie.py
+++ src/build_movie.py
@@ -7,6 +7,20 @@ with open('consts.json', 'r') as f:
     consts = json.load(f)
 
 def main():
+    with open(f'pagefeeds.json', 'r') as f:
+        pagefeeds = json.load(f)
+
+    video_clips = []
+    for i, page in enumerate(sorted(glob.glob('pages/novel*.png'))):
+        duration = 1
+        clip = ImageClip(page) \
+            .set_start(i * (duration - consts['cross_fade_time'])) \
+            .set_duration(duration) \
+            .crossfadein(consts['cross_fade_time']) \
+            .crossfadeout(consts['cross_fade_time'])
+        video_clips.append(clip)
+    video_clip = CompositeVideoClip(video_clips, bg_color=(255, 255, 255))
+
     voice_clips = []
     s = consts['start_voice_interval']
     for voice in sorted(glob.glob('voices/*.mp3')):
@@ -16,20 +30,9 @@ def main():
         s += clip.duration + consts['voice_interval']
     voice_clip = CompositeAudioClip(voice_clips)
 
-    video_clips = []
-    for i, page in enumerate(sorted(glob.glob('pages/novel*.png'))):
-        duration = 1
-        clip = ImageClip(page) \
-            .set_start(i * (duration - consts['music_fadeout_time'])) \
-            .set_duration(duration) \
-            .crossfadein(consts['music_fadeout_time']) \
-            .crossfadeout(consts['music_fadeout_time'])
-        video_clips.append(clip)
-    video_clip = CompositeVideoClip(video_clips, bg_color=(255, 255, 255))
-
     music_clip = AudioFileClip('music.mp3') \
         .audio_loop(duration=video_clip.duration) \
-        .audio_fadeout(duration=5) \
+        .audio_fadeout(duration=consts['music_fadeout_time']) \
         .volumex(consts['music_volume'])
 
     audio_clip = CompositeAudioClip([voice_clip, music_clip])
