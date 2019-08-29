4eb78831ed15a55c6d428490422cec76183210ff
m_cre <mc@mchs-u.net>
2019-08-29 23:39:03 +0900

    pollyの実行タイミングをyキーを打ったあとにした

--- src/ssml2voice.py
+++ src/ssml2voice.py
@@ -54,44 +54,42 @@ def main(aws_access_key_id, aws_secret_access_key):
     polly = session.client('polly')
     bucket = session.resource('s3').Bucket(consts['s3_bucket_name'])
 
-    tasks = []
+    pending_tasks = []
     for ssml in sorted(glob.glob('ssml/*.xml')):
-        try:
-            with open(ssml, 'r') as f:
-                text = f.read()
-                md5 = hashlib.md5(text.encode()).hexdigest()
-                cache_path = f'cache/{consts["voice_id"]}/{md5}'
-                if not os.path.isfile(f'{cache_path}/voice.mp3') or not os.path.isfile(f'{cache_path}/voice.json'):
-                    os.makedirs(cache_path, exist_ok=True)
-                    shutil.copy(ssml, f'{cache_path}/voice.xml')
-                    tasks.append(start_task(ssml, f'{cache_path}/voice.mp3', polly, text, 'mp3'))
-                    tasks.append(start_task(ssml, f'{cache_path}/voice.json', polly, text, 'json'))
-                else:
-                    shutil.copy(f'{cache_path}/voice.mp3', f'voices/{basename(ssml)}.mp3')
-                    shutil.copy(f'{cache_path}/voice.json', f'marks/{basename(ssml)}.json')
-        except Exception as e:  # エラーはとりあえずスルーする
-            print(e)
+        with open(ssml, 'r') as f:
+            text = f.read()
+            md5 = hashlib.md5(text.encode()).hexdigest()
+            cache_path = f'cache/{consts["voice_id"]}/{md5}'
+            if not os.path.isfile(f'{cache_path}/voice.mp3') or not os.path.isfile(f'{cache_path}/voice.json'):
+                os.makedirs(cache_path, exist_ok=True)
+                shutil.copy(ssml, f'{cache_path}/voice.xml')
+                pending_tasks.append({'ssml': ssml, 'cache': f'{cache_path}/voice.mp3', 'text': text, 'format': 'mp3'})
+                pending_tasks.append({'ssml': ssml, 'cache': f'{cache_path}/voice.json', 'text': text, 'format': 'json'})
+            else:
+                shutil.copy(f'{cache_path}/voice.mp3', f'voices/{basename(ssml)}.mp3')
+                shutil.copy(f'{cache_path}/voice.json', f'marks/{basename(ssml)}.json')
 
-    with open('polly_tasks.json', 'w') as f:  # 落ちた時のために！
-        json.dump(tasks, f, ensure_ascii=False, indent=2)
-
-    if len(tasks) > 10:
+    if len(pending_tasks) > 10:
         print('自動実行の場合はエラーが発生します。エラーが発生した場合は手動で実行してください。手動実行方法↓')
         print(f'docker run --rm -it -v $PWD/work:/work lp-python /bin/sh -c "python ssml2voice.py {aws_access_key_id} {aws_secret_access_key}"')
         print('cp -r ./work/cache/* ./cache\n')
         chars_len = 0
-        for task in tasks:
-            with open(task['ssml_path'], 'r') as f:
+        for pending_task in pending_tasks:
+            with open(pending_task['ssml'], 'r') as f:
                 ssml = f.read()
                 chars_len += len(PATTERNS['tag'].sub('', ssml))
         usd = chars_len * 4e-6
         jpy = usd * 106
         print(f'{chars_len:,} chars -> {usd:.2f} USD ≒ {jpy:.1f} JPY')
 
-        input_text = input(f'polly変換数が10を超えました({len(tasks)})。そのまま続ける場合はyを入力してください\n input: ')
+        input_text = input(f'polly変換数が10を超えました({len(pending_tasks)})。そのまま続ける場合はyを入力してください\n input: ')
         if input_text != 'y':
             print('終了します')
             return
+
+    tasks = [start_task(t['ssml'], t['cache'] ,polly, t['text'], t['format']) for t in pending_tasks]
+    with open('polly_tasks.json', 'w') as f:  # 落ちた時のために！
+        json.dump(tasks, f, ensure_ascii=False, indent=2)
 
     print(f'polly: {len(tasks) // 2} * 2 tasks')
     for task in tasks:
@@ -99,18 +97,21 @@ def main(aws_access_key_id, aws_secret_access_key):
             response = polly.get_speech_synthesis_task(TaskId=task['task_id'])
             st = response['SynthesisTask']['TaskStatus']
             if st == 'completed':
-                print(f'polly: success ({task["format"]}, {task["name"]})')
-                if task['format'] == 'mp3':
-                    p = f'voices/{task["name"]}.mp3'
-                    bucket.download_file(f'{task["s3_basename"]}.mp3', p)
-                    shutil.copy(p, task['cache_path'])
-                if task['format'] == 'json':
-                    p1 = f'marks_tmp/{task["name"]}.marks'
-                    p2 = f'marks/{task["name"]}.json'
-                    bucket.download_file(f'{task["s3_basename"]}.marks', p1)
-                    with open(p1, 'r', encoding='utf-8') as fr, open(p2, 'w', encoding='utf-8') as fw:
-                        json.dump([json.loads(line) for line in fr.readlines()], fw, ensure_ascii=False, indent=2)
-                    shutil.copy(p2, task['cache_path'])
+                try:
+                    print(f'polly: success ({task["format"]}, {task["name"]})')
+                    if task['format'] == 'mp3':
+                        p = f'voices/{task["name"]}.mp3'
+                        bucket.download_file(f'{task["s3_basename"]}.mp3', p)
+                        shutil.copy(p, task['cache_path'])
+                    if task['format'] == 'json':
+                        p1 = f'marks_tmp/{task["name"]}.marks'
+                        p2 = f'marks/{task["name"]}.json'
+                        bucket.download_file(f'{task["s3_basename"]}.marks', p1)
+                        with open(p1, 'r', encoding='utf-8') as fr, open(p2, 'w', encoding='utf-8') as fw:
+                            json.dump([json.loads(line) for line in fr.readlines()], fw, ensure_ascii=False, indent=2)
+                        shutil.copy(p2, task['cache_path'])
+                except Exception:
+                    pass
                 break
             elif st == 'failed':
                 print(f'polly: failed!!! ({task["format"]}, {task["name"]})')
