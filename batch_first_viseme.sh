e0c99f263e280288a9a32ea6a0badab5ec8c954f
m_cre <mc@mchs-u.net>
2019-10-09 21:52:14 +0900

    tweak

--- batch_first_viseme.sh
+++ batch_first_viseme.sh
@@ -3,7 +3,6 @@ pj=$1
 mkdir -p ./projects/${pj}/tmp
 log="./projects/${pj}/tmp/batch_first_viseme.log"
 ./batch.sh ${pj} x x viseme 2>&1 | tee ${log}
-if [[ "${PIPESTATUS[0]}" == 1 ]] || [[ "$pipestatus[1]" == 1 ]]; then exit 0; fi;
 cp ./work/novel.pdf ./projects/${pj}/tmp/novel.pdf
 cp ./work/novel.tex ./projects/${pj}/tmp/novel.tex
 cp ./work/viseme.pdf ./projects/${pj}/tmp/viseme.pdf
